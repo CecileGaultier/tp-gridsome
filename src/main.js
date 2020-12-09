1865b77381929d8480df1ebf141cd93cc78abcaf
CecileGaultier <70898757+CecileGaultier@users.noreply.github.com>
2020-12-09 20:21:37 +0100

    bootstrap

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
+
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "popper.js/dist/popper";
-import "popper.js/dist/popper-utils";
 
 import DefaultLayout from '~/layouts/Default.vue'
 
