7a3bdd45dc050bbbfaa03b503fa6646789ef396b
CecileGaultier <70898757+CecileGaultier@users.noreply.github.com>
2020-12-11 09:57:17 +0100

    page a propos

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,22 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <h1>À propos</h1>
+    <p>
+      Le surf a acquis une importance internationale dans les années 1900 quand le célèbre surfeur hawaïen Duke Kahanamoku remporté la médaille d'or pour la natation aux Jeux olympiques de 1912. Dans le contexte des relations publiques, une tournée de démonstration a été organisée dans le monde, qui a permis à de nombreux pays de découvrir ce sport. En conséquence, le surf a gagné en popularité et en 1964, au cours des premiers championnats du monde de surf à proximité de Sydney, en Australie, l'ISF (International Surfing Federation) a été créé, avec Eduardo Arena élu Président. Cette association a rassemblé toutes les nations de surf et a commencé à organiser des championnats du monde tous les deux ans. En novembre 1976, à Hawaï, l'ISF a fait place à l'ISA (International Surfing Association).
+    </p>
+    <p>
+      L'ISA est l'autorité régissant le monde pour le surf et toutes ses disciplines, y compris bodyboard, kneeboard, longboard, tandem, skimboard et bodysurf. L'ISA régit les sports et travaille à leur développement dans ses pays membres et à l'établissement de nouveaux sites de pratique.
+    </p>
+    <p>
+      L'ISA organise le World Championship événement qualificatif aux Jeux olympiques.
+    </p>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'About us'
+    title: 'À propos'
   }
 }
 </script>
